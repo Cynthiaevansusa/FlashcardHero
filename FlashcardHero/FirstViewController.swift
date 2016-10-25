a58283762ff3931ef82531e9c90ccdcbc6b19af0
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-10-24 22:48:04 -0400

    Did some basic Charts checking.  Without doing a full test, it appears the library is installed.

--- FlashcardHero/FirstViewController.swift
+++ FlashcardHero/FirstViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import Charts
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+
     }
 
 
