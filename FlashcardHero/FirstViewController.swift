5be60c71dd6ca2344868e7461eff86efcd70949f
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-10-24 21:20:46 -0400

    Initial Commit

---
+++ FlashcardHero/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlashcardHero
+//
+//  Created by Jacob Foster Davis on 10/24/16.
+//  Copyright © 2016 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlashcardHero/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FlashcardHero
+//
+//  Created by Jacob Foster Davis on 10/24/16.
+//  Copyright © 2016 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
