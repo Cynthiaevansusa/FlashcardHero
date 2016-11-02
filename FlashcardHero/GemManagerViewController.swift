8fbd2e390d58cb7184a32f6a8730aa7a5f43f7c8
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-11-02 00:06:44 -0400

    added GemManager as a CoreData child

--- FlashcardHero/GemManagerViewController.swift
+++ FlashcardHero/GemManagerViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class GemManagerViewController: CoreDataQuizletTableViewController, UISearchBarDelegate, QuizletSetSearchResultIngesterDelegate {
 
@@ -35,6 +36,54 @@ class GemManagerViewController: CoreDataQuizletTableViewController, UISearchBarD
 
     func addToDataModel(_ QuizletSetSearchResults: [QuizletSetSearchResult]) {
         //take the array of QuizletSetSearchResults and add to CoreData
+    }
+    
+    /******************************************************/
+    /******************* Model Operations **************/
+    /******************************************************/
+    //MARK: - Model Operations
+    
+    func setupFetchedResultsController(){
+        
+        //set up stack and fetchrequest
+        // Get the stack
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let stack = delegate.stack
+        
+        // Create Fetch Request
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "QuizletSet")
+        
+        fr.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false),NSSortDescriptor(key: "id", ascending: true)]
+        
+        // So far we have a search that will match ALL notes. However, we're
+        // only interested in those within the current notebook:
+        // NSPredicate to the rescue!
+        
+//        let pred = NSPredicate(format: "pin = %@", argumentArray: [self.pin!])
+//        
+//        fr.predicate = pred
+        
+        // Create FetchedResultsController
+        let fc = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.fetchedResultsController = fc
+        
+    }
+    
+    func fetchModelQuizletSets() -> [QuizletSet] {
+        return fetchedResultsController!.fetchedObjects as! [QuizletSet]
+    }
+    
+    func setupTableView() {
+        //if there are stored QuizletSets in the model, load them into the collection view
+        setsToDisplay = fetchModelQuizletSets()
+        
+        //if the contents is empty, the load some contents
+//        if setsToDisplay.count < 1 {
+//            flickrGetPhotosNearPin(radius: 5.0, tryAgain: true)
+//        }
+        
+        
     }
     
     /******************************************************/
