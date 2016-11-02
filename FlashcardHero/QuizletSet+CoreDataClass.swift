c23f73773b7bc1cc4920cb7786dbad48406475e0
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-11-01 22:40:42 -0400

    reattempt to get skeleton of QuizletSet CoreData

--- FlashcardHero/QuizletSet+CoreDataClass.swift
+++ FlashcardHero/QuizletSet+CoreDataClass.swift
@@ -4,13 +4,12 @@
 //
 //  Created by Jacob Foster Davis on 11/1/16.
 //  Copyright © 2016 Zero Mu, LLC. All rights reserved.
-//  This file was automatically generated and should not be edited.
 //
 
 import Foundation
 import CoreData
 
-@objc(QuizletSet)
+
 public class QuizletSet: NSManagedObject {
 
 }
--- FlashcardHero/QuizletSet+CoreDataProperties.swift
+++ FlashcardHero/QuizletSet+CoreDataProperties.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jacob Foster Davis on 11/1/16.
 //  Copyright © 2016 Zero Mu, LLC. All rights reserved.
-//  This file was automatically generated and should not be edited.
 //
 
 import Foundation
@@ -22,21 +21,21 @@ extension QuizletSet {
     @NSManaged public var createdBy: String?
     @NSManaged public var createdDate: NSDate?
     @NSManaged public var creatorId: Int64
-    @NSManaged public var setDescription: String?
     @NSManaged public var editable: String?
     @NSManaged public var hasAccess: Bool
-    @NSManaged public var id: Int64
     @NSManaged public var hasImages: Bool
+    @NSManaged public var id: Int64
     @NSManaged public var langDefinitions: String?
     @NSManaged public var langTerms: String?
     @NSManaged public var modifiedDate: NSDate?
     @NSManaged public var passwordEdit: Bool
     @NSManaged public var passwordUse: Bool
     @NSManaged public var publishedDate: NSDate?
+    @NSManaged public var setDescription: String?
     @NSManaged public var subjects: String?
+    @NSManaged public var term_count: Int64
     @NSManaged public var title: String?
     @NSManaged public var url: String?
-    @NSManaged public var term_count: Int64
     @NSManaged public var visibility: String?
 
 }
